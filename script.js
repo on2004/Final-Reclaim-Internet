b66bea6a893b184b21df3bc8bbd4358c87ce069b
on2004 <on2047@nyu.edu>
2024-12-03 18:20:05 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Add a dynamic animation when buttons are clicked
-document.querySelectorAll('.explore-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    alert("Stay tuned! This section is under construction.");
-  });
-});
