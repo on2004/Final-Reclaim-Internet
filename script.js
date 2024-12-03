4667965659a865d5c3e30b24566e5ad64a3040c6
on2004 <on2047@nyu.edu>
2024-12-03 18:24:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a dynamic animation when buttons are clicked
+document.querySelectorAll('.explore-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    alert("Stay tuned! This section is under construction.");
+  });
+});
